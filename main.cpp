b6ac89c6b86efc8e155b6e5538f470e315cb7737
jiangyuhao <591313@auto.nd.com.cn>
2024-07-25 11:26:20 +0800

    [feat] main.cpp打印时间

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!";
+    cout << "Hello World!" << endl;
+    time_t now = time(nullptr);
+    tm* curr_tm = localtime(&now);  // 返回的结构体存储位置未知，不知何时释放，因此推荐使用安全版本。
+    char time[80] = {0};
+    strftime(time, 80, "%Y-%m-%d %H:%M:%S", curr_tm);
+    cout << time << endl;
     return 0;
 }
