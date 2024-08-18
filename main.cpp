ded365806288bced5f5ab1579a5772e1c704f6a4
happyjyh <CodeTracker@outlook.com>
2024-08-18 18:37:42 +0800

    [feat]:添加单例模板以及代码风格MD文件

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
 #include<iostream>
+#include "Singleton.h"
 using namespace std;
+
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    time_t now = time(nullptr);
-    tm* curr_tm = localtime(&now);  // 返回的结构体存储位置未知，不知何时释放，因此推荐使用安全版本。
-    char time[80] = {0};
-    strftime(time, 80, "%Y-%m-%d %H:%M:%S", curr_tm);
-    cout << time << endl;
+
     return 0;
 }
