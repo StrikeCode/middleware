d0750a2404ddbd5394c08a12c7d2dc9c328d14b6
happyjyh <CodeTracker@outlook.com>
2024-11-03 22:49:42 +0800

    feat: 可以等待的线程安全的栈实现与测试

--- ThreadSafeStack.h
+++ ThreadSafeStack.h
@@ -66,6 +66,98 @@ public:
     }
 };
 
+template<typename T>
+class threadsafe_stack_waitable
+{
+private:
+    std::stack<T> data;
+    mutable std::mutex m; // protect data
+    std::condition_variable cv;
+public:
+    threadsafe_stack_waitable(){}
+
+    threadsafe_stack_waitable(const threadsafe_stack_waitable &other) // copy ctor
+    {
+        std::lock_guard<std::mutex> lock(other.m);
+        data = other.data;
+    }
+
+    threadsafe_stack_waitable& operator=(const threadsafe_stack_waitable&) = delete;
+
+    void push(T new_value)
+    {
+        std::lock_guard<std::mutex> lock(m);
+        data.push(std::move(new_value));
+        cv.notify_one();
+    }
+
+    std::shared_ptr<T> wait_and_pop()
+    {
+        std::unique_lock<std::mutex> lock(m);
+        // unlock + block until true
+        cv.wait(lock, [this](){
+            if(data.empty())
+            {
+                return false;
+            }
+            return true;
+        });
+
+        std::shared_ptr<T> const res(
+                std::make_shared<T>(std::move(data.top()))
+                );
+        data.pop();
+        return res;
+    }
+
+    void wait_and_pop(T& value)
+    {
+        std::unique_lock<std::mutex> lock(m);
+        // unlock + block until true
+        cv.wait(lock, [this](){
+            if(data.empty())
+            {
+                return false;
+            }
+            return true;
+        });
+
+        value = std::move(data.top());
+        data.pop();
+    }
+
+    bool empty() const
+    {
+        std::lock_guard<std::mutex> lock(m);
+        return data.empty();
+    }
+
+    bool try_pop(T& value)
+    {
+        std::lock_guard<std::mutex> lock(m);
+        if(data.empty())
+        {
+            return false;
+        }
+
+        value = std::move(data.top());
+        data.pop();
+        return true;
+    }
+
+    std::shared_ptr<T> try_pop()
+    {
+        std::lock_guard<std::mutex> lock(m);
+        if(data.empty())
+        {
+            return std::shared_ptr<T>();
+        }
+
+        std::shared_ptr<T> res(std::make_shared<T>(std::move(data.top())));
+        return res;
+
+    }
+};
 
 
 #endif //MIDDLEWARES_THREADSAFESTACK_H
--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 #include <cstdlib>
 #include <ctime>
 #include "Singleton.h"
@@ -7,23 +7,75 @@
 #include "ThreadSafeStack.h"
 
 using namespace std;
+#if 0 // 测试 线程安全的栈的并发操作，两个线程取，一个线程生产
+class MyClass
+{
+public:
+    MyClass(int data):_data(data){}
+    MyClass(const MyClass &mc):_data(mc._data){}
+    MyClass(MyClass &&mc):_data(mc._data)
+    {}
+
+    friend std::ostream& operator <<(std::ostream &os, const MyClass &mc)
+    {
+        os << mc._data;
+        return os;
+    }
+private:
+    int _data;
+};
+std::mutex mtx_cout; // 保证输出清晰的互斥量
+void PrintMyClass(std::string consumer, std::shared_ptr<MyClass> data)
+{
+    std::lock_guard<std::mutex> lock(mtx_cout);
+    std::cout << consumer << " pop data success , data is " << (*data) << std::endl;
+}
+
 void TestThreadSafeStack()
 {
-    std::srand(static_cast<unsigned>(std::time(nullptr)));
-    ThreadSafeStack<int> stk;
-    for(int i = 0; i < 10; ++i)
-    {
-        stk.push(std::rand() % 100);
-    }
-    int val;
-    while(!stk.empty())
-    {
-        std::shared_ptr<int> pVal = stk.pop();
-        std::cout << *pVal << " " << std::endl;
-    }
+    threadsafe_stack_waitable<MyClass> stack;
+
+    std::thread consumer1(
+            [&]()
+            {
+                for(;;)
+                {
+                    std::shared_ptr<MyClass> data = stack.wait_and_pop();
+                    PrintMyClass("consumer1", data);
+                }
+            }
+            );
+
+    std::thread consumer2(
+            [&]()
+            {
+                for(;;)
+                {
+                    std::shared_ptr<MyClass> data = stack.wait_and_pop();
+                    PrintMyClass("consumer2", data);
+                }
+            }
+    );
+
+    std::thread producer(
+            [&]()
+            {
+                for(int i = 0; i < 100; i++)
+                {
+                    MyClass mc(i);
+                    stack.push(std::move(mc));
+                }
+            }
+            );
+
+    consumer1.join();
+    consumer2.join();
+    producer.join();
 }
+#endif
+
 int main()
 {
-    TestThreadSafeStack();
+//    TestThreadSafeStack();
     return 0;
 }
