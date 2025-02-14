4036e08c6d3f01f413b8dce07c6c346a7e151ef1
Jiangyao <code980313@gmail.com>
2025-02-14 12:45:20 +0800

    [feat] 日志库第一版提交，日志数量较少的时候暂时不会持久化到磁盘

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "../include/ThreadSafeStack.h"
 #include "../include/LockFreeQue.h"
 #include "../include/ThreadPool.h"
+#include "../include/RingLog.h"
 
 using namespace std;
 
@@ -159,12 +160,49 @@ void TestThreadSafeQueHt()
     producer.join();
 }
 #endif
+
+void TestRingLogSingleThread()
+{
+    LOG_INIT("log", "myname", 3);
+    for (int i = 0;i < 1e6; ++i)
+    {
+        LOG_ERROR("my number is number my number is my number is my number is my number is my number is my number is %d", i);
+    }
+}
+
+int64_t get_current_millis(void) {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (int64_t)tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
+
+void* thdo(void* args)
+{
+    for (int i = 0;i < 1e7; ++i)
+    {
+        LOG_ERROR("my number is number my number is my number is my number is my number is my number is my number is %d", i);
+    }
+}
+
+void TestRingLogMultiThread()
+{
+    LOG_INIT("log", "myname", 3);
+    pthread_t tids[5];
+    for (int i = 0;i < 5; ++i)
+        pthread_create(&tids[i], NULL, thdo, NULL);
+
+    for (int i = 0;i < 5; ++i)
+        pthread_join(tids[i], NULL);
+}
 int main()
 {
 //    TestThreadSafeStack();
 //    TestThreadSafeQue();
 //    TestThreadSafeQueHt();
 //    TestLockFreeQue();
-    TestThreadPool();
+//    TestThreadPool();
+    TestRingLogSingleThread();
+//    TestRingLogMultiThread();
+
     return 0;
 }
